31700585c436bf856d40a85c2f326d2a8b947825
Jungwoo Lee <188685705+howookingking@users.noreply.github.com>
2026-01-14 15:14:34 +0900

    Update formatted-mono-date.tsx

--- components/common/formatted-mono-date.tsx
+++ components/common/formatted-mono-date.tsx
@@ -16,6 +16,7 @@ export default function FormattedMonoDate({
     <time
       dateTime={new Date(date).toISOString()}
       className={cn('pt-[1px] font-mono', className)}
+      suppressHydrationWarning
     >
       {format(new Date(date), withTime ? 'yy.MM.dd HH:mm' : 'yy.MM.dd')}
     </time>
