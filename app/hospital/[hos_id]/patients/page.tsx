78d1fd5f54b7ee4f331a5e2952cdb6bcc0d4af88
Jungwoo Lee <188685705+howookingking@users.noreply.github.com>
2026-01-14 15:41:11 +0900

    엑셀 업로드 ui

--- app/hospital/[hos_id]/patients/page.tsx
+++ app/hospital/[hos_id]/patients/page.tsx
@@ -1,6 +1,7 @@
 import MobileTitle from '@/components/common/mobile-title'
 import SearchPatientContainer from '@/components/common/patients/search/search-patient-containter'
 import { PawPrintIcon } from 'lucide-react'
+import { Suspense } from 'react'
 
 export default async function HospitalPatientsPage(props: {
   params: Promise<{ hos_id: string }>
@@ -8,7 +9,7 @@ export default async function HospitalPatientsPage(props: {
   const params = await props.params
 
   return (
-    <>
+    <Suspense>
       <MobileTitle icon={PawPrintIcon} title="환자목록" />
 
       <SearchPatientContainer
@@ -16,6 +17,6 @@ export default async function HospitalPatientsPage(props: {
         isIcu={false}
         setIsIcuRegisterDialogOpen={undefined}
       />
-    </>
+    </Suspense>
   )
 }
